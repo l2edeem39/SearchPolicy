acb6ef5db19512266ce270e3e0b65011344ee381
prodigy59 <prodigy59@R3BM651178>
2023-03-28 10:28:25 +0700

    [ADD] : structure interface

---
+++ Service/CmiService.cs
@@ -0,0 +1,16 @@
+﻿using SearchPolicy.Api.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchPolicy.Api.Service
+{
+    public class CmiService : ICmiService
+    {
+        public string SearchCmi()
+        {
+            return "123456789";
+        }
+    }
+}
